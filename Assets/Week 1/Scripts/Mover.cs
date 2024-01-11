3b7cef9fcb943b5d522fbf78282259db0d6659d2
Osama Hussein <osamahusseinn2@gmail.com>
2024-01-11 15:19:37 -0500

    I completed Task 2 b where I got the tank to fire a missile based on the keyboard input

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -7,6 +7,11 @@ public class Mover : MonoBehaviour
     // Create a predetermined float speed of the tank
     float speedOfTank = 5.0f;
 
+    // Create a public missile prefab game object
+    public GameObject missilePrefab;
+
+    public Transform barrel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +26,12 @@ public class Mover : MonoBehaviour
 
         // Move the tank around horizontally depending on the keyboard input (pressed by the player) and the speed of the tank float
         transform.Translate(keyboardInput * speedOfTank * Time.deltaTime, 0, 0);
+        
+        // If the player presses space key
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Makes the missile appear
+            Instantiate(missilePrefab, barrel.position, barrel.rotation);
+        }
     }
 }
