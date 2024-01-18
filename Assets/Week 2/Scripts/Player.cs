88f035cfbef15b331355cd64b4f871cd6bd11df4
Osama Hussein <osamahusseinn2@gmail.com>
2024-01-18 13:58:14 -0500

    Player is moving on keyboard input and when the player collides with the wall, the console will print a message saying that the player collided into the wall

---
+++ Assets/Week 2/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Create a direction in 2d vector for the player sprite
+    Vector2 direction;
+
+    public Rigidbody2D rigidbody;
+    public float force = 150f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction * force * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("The player has hit something!");
+    }
+}
