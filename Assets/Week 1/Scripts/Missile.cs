484a5fb6759bbf1502b951f51b761919dd5cee30
Osama Hussein <osamahusseinn2@gmail.com>
2024-01-18 14:20:21 -0500

    I completed Task 1 where the collisions for the player, walls and missiles are happening

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -6,17 +6,30 @@ public class Missile : MonoBehaviour
 {
     // Create a predetermined float for the missile speed
     float missileSpeed = 3.0f;
+    Rigidbody2D rigidbody;
+    Vector2 direction = new Vector2(10, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Move the missile to the right on each frame when the player presses the space key to fire the missile
-        transform.Translate(missileSpeed * Time.deltaTime, 0, 0);
+        //transform.Translate(missileSpeed * Time.deltaTime, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Missile hit something!");
+        Destroy(gameObject);
     }
 }
