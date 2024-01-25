b37221771af0df69faaa610191dd810961bb9c6c
Osama Hussein <osamahusseinn2@gmail.com>
2024-01-25 16:40:56 -0500

    I completed Task 4b where I created a trigger for the car when the player collides with the oil and made it spin out if the player is moving on the oil (it'll also spin the car in circles if the player is still on the oil but is not moving the car)!

--- Assets/Week 3/Scripts/Car.cs
+++ Assets/Week 3/Scripts/Car.cs
@@ -12,6 +12,8 @@ public class Car : MonoBehaviour
 
     public float maxSpeed;
 
+    //public GameObject oil, oil2, oil3;
+
     Rigidbody2D rigidbody;
 
     // Start is called before the first frame update
@@ -36,6 +38,30 @@ public class Car : MonoBehaviour
         if (rigidbody.velocity.magnitude < maxSpeed)
         {
             rigidbody.AddForce(force);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "oil" || collision.gameObject.name == "oil (1)" || collision.gameObject.name == "oil (2)")
+        {
+            Debug.Log("You entered the oil slick!");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "oil" || collision.gameObject.name == "oil (1)" || collision.gameObject.name == "oil (2)")
+        {
+            rigidbody.AddTorque((steeringSpeed * steeringInput - 200) * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "oil" || collision.gameObject.name == "oil (1)" || collision.gameObject.name == "oil (2)")
+        {
+            rigidbody.AddTorque(steeringSpeed * steeringInput * Time.deltaTime);
         }
     }
 }
