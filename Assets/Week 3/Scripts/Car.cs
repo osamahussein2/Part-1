143815bf789d0670f71b5a0c544a876b06116e4f
Osama Hussein <osamahusseinn2@gmail.com>
2024-01-25 15:33:44 -0500

    I completed Task 3 where I was able to get the car moving properly! And I fixed the background collision issue by removing the colliders and rigidbody component from the background layer.

---
+++ Assets/Week 3/Scripts/Car.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float forwardSpeed;
+    float forwardInput;
+
+    public float steeringSpeed;
+    float steeringInput;
+
+    public float maxSpeed;
+
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        steeringInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddTorque(steeringSpeed * -steeringInput * Time.deltaTime);
+
+        Vector2 force = transform.up * forwardSpeed * forwardInput * Time.deltaTime;
+
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(force);
+        }
+    }
+}
